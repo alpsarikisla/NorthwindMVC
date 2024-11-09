eb59686da6597185ccafe19ff303f358958fbce8
Alp Sarıkışla <alpsarikisla@hotmail.com>
2024-11-09 10:27:05 +0300

    Initial Commit

---
+++ NorthwindIleMVC/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using NorthwindIleMVC.Models;
+using NorthwindIleMVC.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthwindIleMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        NORTHWNDEntities db = new NORTHWNDEntities();
+        
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Employees employee = db.Employees.FirstOrDefault(e => e.UserName == model.UserName && e.Password == model.Password);
+                if (employee != null)
+                {
+                    Session["employee"] = employee;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.error = "Kullanıcı bulunamadı";
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ NorthwindIleMVC/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NorthwindIleMVC.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Kullanıcı Adı Boş Bırakılamaz")]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Şifre Boş Bırakılamaz")]
+        public string Password { get; set; }
+    }
+}
